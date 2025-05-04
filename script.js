0526b6f5705c6d094d4712da7d9836e8cb6d8d3d
Defalter <103436489+DefalterXD@users.noreply.github.com>
2025-05-05 00:50:29 +0500

    feat: add input from button numbers

--- script.js
+++ script.js
@@ -1,57 +1,54 @@
-// PLAN:
-
-// Getting input through buttons and keyboard
-// Output the input through the output container
-// And make only one operation per screen
-// Adding basic operations for calculator
-
-
 // INIT outputContainer for output element
 const outputContainer = document.querySelector('.output__container');
 outputContainer.textContent = 0; // View initial number
 // INIT calculatorContainer for event delegation purpose
 const calculatorContainer = document.querySelector('.calculator__container');
-// INIT calculatorBtns array of:
-// SET an array for clear buttons (and % operator)
-// SET an array for number buttons
-// SET an array for operator buttons
 // INIT variable for the first number input
 let firstNumber = 0;
 // INIT variable for the operator input
 let operator = null;
 // INIT variable for the second number input
 let secondNumber = 0;
-// INIT variable for all the number output
-const numberFromData = [];
+// INIT array variable for all the number output
+let numberFromData = [];
 
-// DEFINE isNumber function expression
+// DEFINE isNumberWasInputed function expression
 const isNumberWasInputed = function isNumberWasInputedAtTheStartOfTheExpression(input) {
-    // IF input.dataset.key equals to zero 
-    // AND (numberFromData array is not empty AND numberFromData array length is less than nine)
-    if (Number(input.dataset.key) === 0 && (numberFromData.length !== 0 && numberFromData.length < 9)) {
+    // IF input equals to zero AND firstNumber is still zero
+    if (numberFromData.includes('0') && numberFromData.length < 2) {
+        numberFromData.pop();
+        numberFromData.push(input);
+        outputContainer.textContent = numberFromData.join('');
+    }
+    // ELSE IF input equals to zero 
+    else if (Number(input) === 0) {
         // THEN push the number into array
-        numberFromData.push(input.dataset.key);
+        numberFromData.push(input);
         // THEN output the result from all elements and delete the commas
         outputContainer.textContent = numberFromData.join('');
     }
-    // ELSE IF numberFromData array length is less than nine 
-    // AND the input data is a number AND zero exception from input.dataset.key
-    else if (numberFromData.length < 9 && Number(input.dataset.key)) {
+    // ELSE IF the input data is a number 
+    else if (Number(input)) {
         // THEN push the number into array
-        numberFromData.push(input.dataset.key);
-        // THEN output the result from all elements and delete the commas
+        numberFromData.push(input);
+        // PRINT out output the result from all elements and delete the commas
         outputContainer.textContent = numberFromData.join('');
     }
 };
 
-
-
+// DEFINE calculatorFunctions function expression
+const calculatorFunctions = function calculatorFunctionsThroughTheEvents(event) {
+    const dataFromBtn = event;
+    // IF dataFromBtn is undefined THEN return
+    if (!dataFromBtn) return; // To prevent appearing in console 'undefined' whenever clicked outside the layout
+    // IF dataFromBtn is a number
+    if (Number(dataFromBtn) || Number(dataFromBtn) == 0) {
+        // THEN invoke isNumber function
+        isNumberWasInputed(dataFromBtn);
+    }
+};
 
 // ADD EVENT LISTENER delegation to any button clicked
 calculatorContainer.addEventListener('click', (event) => {
-    // TODO: make condition whether event.target is the operation or a number
-
-    isNumberWasInputed(event.target);
-    // PRINT out clicked number button to output
-    // outputContainer.textContent = event.target.textContent;
+    calculatorFunctions(event.target.dataset.key);
 });
