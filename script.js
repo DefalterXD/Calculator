1f9acc2c84d91fa3cbd2866546b74a8455c49430
Defalter <103436489+DefalterXD@users.noreply.github.com>
2025-05-17 00:08:17 +0500

    refactor: delete unnecessary comments

--- script.js
+++ script.js
@@ -1,71 +1,45 @@
-// INIT outputContainer for output element
 const outputContainer = document.querySelector('.output__container');
 outputContainer.textContent = 0; // View initial number
-// INIT calculatorContainer for event delegation purpose
 const calculatorContainer = document.querySelector('.calculator__container');
-// INIT variable for the first number input
 let firstNumber = 0;
-// INIT variable for the operator input
 let operator = null;
-// INIT variable for the second number input
 let secondNumber = 0;
-// INIT array variable for all the number output
 let numberFromData = [];
-// INIT array variable for stored result
 let numberFromResultOperation = [];
-// INIT array variable for all operations
 const operationsData = ['/', '*', '-', '+', '='];
 
-// DEFINE isNumberWasInputed function expression
 const isNumberWasInputed = function isNumberWasInputedAtTheStartOfTheExpression(input) {
-    // IF input equals to zero AND firstNumber is still zero
     if (numberFromData.includes('0') && numberFromData.length < 2) {
         numberFromData.pop();
         numberFromData.push(input);
         outputContainer.textContent = numberFromData.join('');
     }
-    // ELSE IF input equals to zero 
     else if (Number(input) === 0 && numberFromData.length < 9) {
-        // THEN push the number into array
         numberFromData.push(input);
-        // THEN output the result from all elements and delete the commas
         outputContainer.textContent = numberFromData.join('');
     }
-    // ELSE IF the input data is a number 
     else if (Number(input) && !numberFromData.includes('.') && numberFromData.length < 9) {
-        // THEN push the number into array
         numberFromData.push(input);
-        // PRINT out output the result from all elements and delete the commas
         outputContainer.textContent = numberFromData.join('');
-    } 
+    }
     else if (Number(input) && numberFromData.includes('.') && numberFromData.length < 9) {
-         // THEN push the number into array
-         numberFromData.push(input);
-         // PRINT out output the result from all elements and delete the commas
-         outputContainer.textContent = numberFromData.join('');
+        numberFromData.push(input);
+        outputContainer.textContent = numberFromData.join('');
     }
 };
 
-// DEFINE isOperatorWasInputed function expression
 const isOperatorWasInputed = function isOperatorWasInputedInTheMiddleOfTheExpressionAndEvaluatedIt(input) {
-    // INVOKE gatheringNumberFromInput to get value for numbers 
     gatheringNumberFromInput();
 
-    // IF input is equal operation AND two numbers isn't empty
     if (input !== '=' && firstNumber !== 0) {
-        // INVOKE whichOperator() with operator passed argument
         whichOperator(operator);
-        // SET result from evaluation to backup variable
         numberFromResultOperation = firstNumber.toString().split('');
         isNumberBeyondLimit(firstNumber);
 
     }
-    // ELSE IF input isn't equal operator AND two numbers isn't empty 
     else if (input === '=' && firstNumber !== 0) {
 
-        // THEN INVOKE whichOperator() with operator passed argument
         whichOperator(operator);
-        // SET result from evaluation to backup variable
         numberFromResultOperation = firstNumber.toString().split('');
         isNumberBeyondLimit(firstNumber);
     }
@@ -75,120 +49,81 @@ const isOperatorWasInputed = function isOperatorWasInputedInTheMiddleOfTheExpres
 
 };
 
-// DEFINE gatheringNumberFromInput function expression
 const gatheringNumberFromInput = function gatheringNumberFromInputForExpressionEvaluation() {
-    // IF numberFromData isn't empty AND firstNumber equals to zero
     if ((numberFromData.length) && firstNumber === 0) {
-        // THEN assign numberFromData to firstNumber
         firstNumber = Number(numberFromData.join(''));
     }
-    // ELSE IF numberFromData isn't empty AND firstNumber isn't equals to zero
     else if ((numberFromData.length) && secondNumber === 0) {
-        // THEN assign numberFromData to secondNumber
         secondNumber = Number(numberFromData.join(''));
     }
-    // ELSE IF numberFromResultOperation isn't empty
     else if ((numberFromResultOperation.length)) {
-        // THEN assign numberFromResultOperation to firstNumber and secondNumber
         firstNumber = Number(numberFromResultOperation.join(''));
         secondNumber = Number(numberFromResultOperation.join(''));
     }
-    // ELSE
     else {
-        // THEN assign firstNumber to secondNumber
         secondNumber = firstNumber;
     }
 
 };
 
 
-// DEFINE whichOperator function expression 
 const whichOperator = function whichOperatorWasInputed(operator) {
-    // CASE string of operator OF
     switch (operator) {
-        // CONDITION string is multiplication: 
         case '*':
-            // THEN assign firstNumber multiplied by secondNumber to firstNumber
             firstNumber = firstNumber * secondNumber;
             break;
-        // CONDITION string is division:
         case '/':
-            // THEN assign firstNumber divided by secondNumber to firstNumber
             firstNumber = firstNumber / secondNumber;
             break;
-        // CONDITION string is addition: 
         case '+':
-            // THEN assign firstNumber added by secondNumber to firstNumber
             firstNumber = firstNumber + secondNumber;
             break;
-        // CONDITION string is subtraction: 
         case '-':
-            // THEN assign firstNumber subtracted by secondNumber to firstNumber
             firstNumber = firstNumber - secondNumber;
             break;
     }
-    // ENDCASE
 };
 
 
-// DEFINE convertNumberToPercent function expression
 const convertNumberToPercent = function convertNumberToPercentEquivalentForExpression() {
-    // IF numberFromData isn't empty
+
     if (numberFromData.length !== 0) {
-        // THEN convert all the numbers from array into Numbers with a join('') method and evaluate into percent 
-        numberFromData = (Number(numberFromData.join('')) / 100).toString().split('') // assign it back into an numberFromData with toString() and split('') methods
-        // PRINT out the number into output 
+        numberFromData = (Number(numberFromData.join('')) / 100).toString().split('');
         isNumberBeyondLimit(Number(numberFromData.join('')));
     }
-    // ELSE
     else {
-        // SET to percent number from result operation
         numberFromResultOperation = (Number(numberFromResultOperation.join('')) / 100).toString().split('');
         isNumberBeyondLimit(Number(numberFromResultOperation.join('')));
     }
 };
 
 
-// DEFINE convertToNegativeOrPositiveNumber function expression
 const convertToNegativeOrPositiveNumber = function convertToNegativeOrPositiveNumberFromExpressionVariable(numbers) {
-    // IF numberFromData isn't empty AND doesn't has minus sign in array
     if ((numbers.length) && (!numbers.includes('-'))) {
-        // THEN add to start of the array minus sign
         numbers.unshift('-');
         isNumberBeyondLimit(Number(numbers.join('')));
     }
-    // ELSE IF  numberFromData isn't empty AND does has minus sign in array
     else if ((numbers.length) && (numbers.includes('-'))) {
-        // THEN remove from the start of the array minus sign
         numbers.shift('-');
         isNumberBeyondLimit(Number(numbers.join('')));
     }
 };
 
-// DEFINE convertNumberIntoDecimal function expression
 const convertNumberIntoDecimal = function convertNumberIntoDecimalFromAnyPlace() {
-    // IF numberFromData includes '.' element
     if (numberFromData.includes('.')) {
-        // THEN just return 
         return;
     }
-    // ELSE IF numberFromData is empty
     else if (!numberFromData.length && !numberFromResultOperation.length) {
-        // THEN push into numberFromData zero and decimal point
         numberFromData.push('0', '.');
         outputContainer.textContent = numberFromData.join('');
     }
-    // ELSE 
     else {
-        // THEN push just the decimal point
         numberFromData.push('.');
         outputContainer.textContent = numberFromData.join('');
     }
 };
 
-// DEFINE isBackspacedWasPressed function expression
 const isBackspacedWasPressed = function isBackspacedWasPressedToClearTheCalculator() {
-    // SET all variables to initial state
     outputContainer.textContent = 0;
     firstNumber = 0;
     operator = null;
@@ -198,28 +133,19 @@ const isBackspacedWasPressed = function isBackspacedWasPressedToClearTheCalculat
 };
 
 
-// DEFINE calculatorFunctions function expression
 const calculatorFunctions = function calculatorFunctionsThroughTheEvents(event) {
     const dataFromBtn = event;
-    // IF dataFromBtn is undefined THEN return
     if (!dataFromBtn) return; // To prevent appearing in console 'undefined' whenever clicked outside the layout
-    // IF dataFromBtn is a number
     if (Number(dataFromBtn) || Number(dataFromBtn) == 0) {
-        // THEN invoke isNumber function
         isNumberWasInputed(dataFromBtn);
     }
-    // ELSE IF dataFromBtn is an operation 
     else if (operationsData.includes(dataFromBtn)) {
         isOperatorWasInputed(dataFromBtn);
     }
-    // ELSE IF dataFromBtn is an percent operation
     else if (dataFromBtn === '%') {
-        // THEN INVOKE convertNumberToPercent function
         convertNumberToPercent();
     }
-    // ELSE IF dataFromBtn is an backtick(minus/plus) operation
     else if (dataFromBtn === '`') {
-        // THEN INVOKE convertToNegativeOrPositiveNumber function
         if ((!numberFromData.length) && (numberFromResultOperation.length)) {
             convertToNegativeOrPositiveNumber(numberFromResultOperation);
         }
@@ -227,30 +153,20 @@ const calculatorFunctions = function calculatorFunctionsThroughTheEvents(event) 
             convertToNegativeOrPositiveNumber(numberFromData);
         }
     }
-    // ELSE IF dataFromBtn is a point
     else if (dataFromBtn === '.') {
-        // THEN INVOKES convertNumberIntoDecimal function
         convertNumberIntoDecimal();
     }
-    // ELSE IF dataFromBtn is an Backspace
     else if (dataFromBtn === 'Backspace') {
-        // THEN INVOKE isBackspacedWasPressed function
         isBackspacedWasPressed();
     }
 };
 
-// DEFINE isNumberBeyondLimit function expression
 const isNumberBeyondLimit = function isNumberBeyondLimitToGreaterOrSmallerValue(number) {
-    // IF number is decimal
     if (number % 1 !== 0) {
-        // THEN INIT digitLength of the decimal
         const digitLength = Math.floor(Math.abs(number)).toString().length;
-        // INIT fractionalLength to fixed the decimal
         const fractionalLength = Math.max(0, 8 - digitLength);
-        // SET number to a fixed decimal position
         number = Number(number.toFixed(fractionalLength));
     }
-    // IF number goes beyond value
     if (number === Infinity || Math.abs(number) > 999999999 || Math.abs(number) < 0.0000000001) {
         outputContainer.textContent = "NO";
         // reset to initial state
@@ -260,19 +176,16 @@ const isNumberBeyondLimit = function isNumberBeyondLimitToGreaterOrSmallerValue(
         numberFromData = [];
         numberFromResultOperation = [];
     }
-    // ELSE print out the number and set second for next expression
     else {
         outputContainer.textContent = number;
         secondNumber = 0;
     }
 }
 
-// ADD EVENT LISTENER delegation to any button clicked
 calculatorContainer.addEventListener('click', (event) => {
     calculatorFunctions(event.target.dataset.key);
 });
 
-// ADD EVENT LISTENER delegation to any key pressed from keyboard
 window.addEventListener('keydown', (event) => {
     calculatorFunctions(event.key);
 });
