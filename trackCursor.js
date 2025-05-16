6c1a21e2def4ec5aa555d7635bedbd5662111c23
Defalter <103436489+DefalterXD@users.noreply.github.com>
2025-05-17 00:15:52 +0500

    refactor: delete unnecessary comments

--- trackCursor.js
+++ trackCursor.js
@@ -1,86 +1,71 @@
 const movingElement = document.querySelector('.calculator__object-round.red');
 
-// GET coordinates of the cursor
 function CursorCoordinates(page) {
     this.coordinateX = page.pageX;
     this.coordinateY = page.pageY;
 }
 
-// INIT for previous cursor position
 let previousX = 0;
 let previousY = 0;
-// INIT pixel for the element to move relatively where cursor direction was
-let coordinateXCount = 0;
-let coordinateYCount = 0;
-// INIT a start point of the inner element
+let coordinateXMove = 0;
+let coordinateYMove = 0;
 let initialX = 0;
 let initialY = 0;
 
-// EVENT LISTENER for the cursor on the computer
 window.addEventListener('mousemove', (event) => {
     const cursor = new CursorCoordinates(event);
     movingElement.style.position = 'relative';
 
-    // IF on x cursor direction move to the right
     if (cursor.coordinateX > previousX) {
-        coordinateXCount++;
+        coordinateXMove++;
     }
-    // ELSE IF on x cursor direction move to the left
     else if (previousX > cursor.coordinateX) {
-        coordinateXCount--;
+        coordinateXMove--;
     }
 
-    // SET coordinate pixel bound to right:4 or right:-4 pixels
-    coordinateXCount = Math.max(Math.min(coordinateXCount, 4), -4);
+    coordinateXMove = Math.max(Math.min(coordinateXMove, 4), -4);
 
-    // MOVE the inner element on X direction
-    movingElement.style.left = initialX + coordinateXCount + 'px';
+    movingElement.style.left = initialX + coordinateXMove + 'px';
 
-    // IF on y cursor direction move to the bottom
     if (cursor.coordinateY > previousY) {
-        coordinateYCount++
+        coordinateYMove++
     }
-    // ELSE IF on y cursor direction move to the top 
     else if (previousY > cursor.coordinateY) {
-        coordinateYCount--;
+        coordinateYMove--;
     }
 
-    // SET coordinate pixel bound to top:4 or top:-4 pixels
-    coordinateYCount = Math.max(Math.min(coordinateYCount, 4), -4);
+    coordinateYMove = Math.max(Math.min(coordinateYMove, 4), -4);
 
-    // MOVE the inner element on Y direction
-    movingElement.style.top = initialY + coordinateYCount + 'px';
+    movingElement.style.top = initialY + coordinateYMove + 'px';
 
-    // SET current position to previous  
     previousX = cursor.coordinateX;
     previousY = cursor.coordinateY;
 
 });
 
-// EVENT LISTENER for the touchpad on the phone
 window.addEventListener('touchmove', (event) => {
     const touch = new CursorCoordinates(event.touches[0]);
     if (touch.coordinateX > previousX) {
-        coordinateXCount++;
+        coordinateXMove++;
     }
     else if (previousX > touch.coordinateX) {
-        coordinateXCount--;
+        coordinateXMove--;
     }
 
-    coordinateXCount = Math.max(Math.min(coordinateXCount, 4), -4);
+    coordinateXMove = Math.max(Math.min(coordinateXMove, 4), -4);
 
-    movingElement.style.left = initialX + coordinateXCount + 'px';
+    movingElement.style.left = initialX + coordinateXMove + 'px';
 
     if (touch.coordinateY > previousY) {
-        coordinateYCount++
+        coordinateYMove++
     }
     else if (previousY > touch.coordinateY) {
-        coordinateYCount--;
+        coordinateYMove--;
     }
 
-    coordinateYCount = Math.max(Math.min(coordinateYCount, 4), -4);
+    coordinateYMove = Math.max(Math.min(coordinateYMove, 4), -4);
 
-    movingElement.style.top = initialY + coordinateYCount + 'px';
+    movingElement.style.top = initialY + coordinateYMove + 'px';
 
     previousX = touch.coordinateX;
     previousY = touch.coordinateY;
